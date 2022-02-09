6b146c19b812f3b1dddd5510efce7266c64843df
xkSilent <fsdfelipe_dias@live.com>
2022-02-09 13:05:39 -0300

    Prejeto Criado

---
+++ src/main/java/com/aula/AutenticaApplication.java
@@ -0,0 +1,13 @@
+package com.aula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutenticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutenticaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aula/AutenticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutenticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
