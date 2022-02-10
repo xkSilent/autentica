ac0ce5248e30d6ae3bdc5c5ae5d732007d8f01fb
xkSilent <fsdfelipe_dias@live.com>
2022-02-10 11:49:05 -0300

    Protegendo a senha com BCrypt

--- src/main/java/com/aula/domain/services/DBService.java
+++ src/main/java/com/aula/domain/services/DBService.java
@@ -4,6 +4,7 @@ import java.text.ParseException;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.aula.domain.Usuario;
@@ -14,12 +15,15 @@ public class DBService {
 	@Autowired
 	UsuarioRepository usuRepository;
 	
+	@Autowired
+	private BCryptPasswordEncoder pe;
+	
 	public void instantiateTestDataBase() throws ParseException{
 		
 		Usuario user1 = new Usuario();
 		user1.setNome("Felipe");
 		user1.setEmail("felipe@hotmail.com");
-		user1.setSenha("12345");
+		user1.setSenha(pe.encode("12345"));
 		
 		usuRepository.saveAll(Arrays.asList(user1));
 	}
